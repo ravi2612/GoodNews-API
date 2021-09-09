58da2a8823839ce0f8b431483cee5966bd174cb3
ravi2612 <ravinavarro26@gmail.com>
2021-09-09 15:26:48 -0300

    first commit

---
+++ GoodNews-API/GoodNews-API/Controllers/NewsListTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NewsListTableViewController.swift
+//  GoodNews-API
+//
+//  Created by Ravi Navarro  on 09/09/21.
+//
+
+import Foundation
+import UIKit
+
+class NewsListTableViewController: UITableViewController {
+    
+}
+
