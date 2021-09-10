1a8381f2bb3bbefc84ac71301d10c5dcf4139592
Raji Navarro <rajinavarro09@gmail.com>
2021-09-10 09:56:02 -0300

    More warnings was added.

--- GoodNews-API/GoodNews-API/Cells/ArticleTableViewCell.swift
+++ GoodNews-API/GoodNews-API/Cells/ArticleTableViewCell.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ArticleTableViewCell: UITableViewCell {
     
+    /*
+     let static cellIdentifier = "ArticleTableViewCell"
+     */
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
     #warning("Cria um método de setupView() com todos dados que precisa, exemplo => setupView(titleText: String?, descriptionText: String?)")
+    
+    /*
+     Solução:
+     
+     func setupView(titleText: String?, descriptionText: String?) {
+         self.titleLabel.text = titleText
+         self.descriptionLabel.text = descriptionText
+     }
+     */
     
 }
--- GoodNews-API/GoodNews-API/Controllers/NewsListTableViewController.swift
+++ GoodNews-API/GoodNews-API/Controllers/NewsListTableViewController.swift
@@ -25,6 +25,14 @@ class NewsListTableViewController: UITableViewController {
         let url = URL(string: "https://newsapi.org/v2/everything?q=Apple&from=2021-09-09&sortBy=popularity&apiKey=c443126044a84e6ab5d3d5a68eb029c1")!
         
         #warning("Essa chamada deveria estar na ViewModel, sendo acessada por meio de um método da viewModel")
+        /*
+         Fluxo certo ===>
+         ViewController (pede pra view model dados dos artigos) ->
+         ViewModel (Pede pra service carregar os dados e espera) ->
+         WebService (Carrega os dados, devolve pra view model) ->
+         ViewModel (Com dados carregados devolve pra view controller) ->
+         ViewController (Atualiza a view) -> FIM
+         */
         WebService().getArticles(url: url) { articles in
             
             if let articles = articles {
@@ -49,8 +57,21 @@ class NewsListTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        
+        // MARK: Sugestão de melhoria 👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇
+        /*
+         if let cell = tableView.dequeueReusableCell(withIdentifier: ArticleTableViewCell.cellIdentifier, for: indexPath) as? ArticleTableViewCell {
+         let articleVM = self.articleListVM.articleAtIndex(indexPath.row)
+         cell.setupView(titleText: articleVM.title, descriptionText: articleVM.description)
+         return cell
+         }
+         
+         /// O método pede um retorno, como o retorno está dentro do if let vc tem que colocar um fora dele padrão.
+         return UITableViewCell()
+         */
+        #warning("Identifier da cell poderia ser uma constante estatica da propria celula, podendo ser acessada como ArticleTableViewCell.cellIDentifier.")
+        #warning("Trocar o guard let por um if let")
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as? ArticleTableViewCell else {
+            #warning("Fatal errors geram crashs no app, não pode deixar aí, em desenvolvimento tudo bem, mas não esquece")
             fatalError("ArticleTableViewCell not found")
         }
        let articleVM = self.articleListVM.articleAtIndex(indexPath.row)
